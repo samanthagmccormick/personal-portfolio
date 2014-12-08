bf55595a87204a82ede7d4e918c7fc662be95e31
Samantha McCormick <samanthagmccormick@gmail.com>
2014-12-08 16:05:33 -0700

    Intitial heroku commit

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', indexController.index);
 app.get('/view/:id', indexController.viewItem);
 
-var server = app.listen(7474, function() {
+var port = process.env.PORT || 7474;
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
